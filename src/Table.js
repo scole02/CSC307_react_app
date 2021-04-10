b4ce714d8b0b5c2a9df92da4d355af2f1968eb13
scole02 <samcole107@gmail.com>
2021-04-09 18:55:10 -0700

    added remove buttons

--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ function TableHeader()  {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -15,8 +16,11 @@ function TableHeader()  {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>         
+          <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+              <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       );
      }
@@ -32,7 +36,7 @@ function TableHeader()  {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
       </table>
     );
 } 
