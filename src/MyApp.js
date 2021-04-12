57a807e8389657c62d3b7afae5efdc684b6e352d
scole02 <samcole107@gmail.com>
2021-04-11 22:03:18 -0700

    frontend now loads characters from bakend on startup

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import Table from './Table'
+import React, {useState, useEffect} from 'react';
+import Table from './Table';
 import Form from './Form';
-
+import axios from 'axios';
 
 const characters = [
   {
@@ -25,6 +25,25 @@ const characters = [
 function MyApp() {   
     const [characters, setCharacters] = useState([]); 
     
+    useEffect(() => {
+      fetchAll().then( result => {
+         if (result)
+            setCharacters(result);
+       });
+    }, [] )
+    
+    async function fetchAll(){
+      try {
+         const response = await axios.get('http://localhost:5000/users');
+         return response.data.users_list;     
+      }
+      catch (error){
+         //We're not handling errors. Just logging into the console.
+         console.log(error); 
+         return false;         
+      }
+    }
+
     function updateList(person) {
       setCharacters([...characters, person]);
     }
