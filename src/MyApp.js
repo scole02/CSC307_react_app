a93204157ffcd2d0b0dd3659227afd5dceb65533
scole02 <samcole107@gmail.com>
2021-04-11 22:13:20 -0700

    submitting name through frontend now makes POST call to backend before updating

--- src/MyApp.js
+++ src/MyApp.js
@@ -25,6 +25,24 @@ const characters = [
 function MyApp() {   
     const [characters, setCharacters] = useState([]); 
     
+    function updateList(person) { 
+      makePostCall(person).then( result => {
+      if (result)
+         setCharacters([...characters, person] );
+      });
+    }
+
+    async function makePostCall(person){
+      try {
+         const response = await axios.post('http://localhost:5000/users', person);
+         return response;
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+    }
+
     useEffect(() => {
       fetchAll().then( result => {
          if (result)
@@ -44,9 +62,6 @@ function MyApp() {
       }
     }
 
-    function updateList(person) {
-      setCharacters([...characters, person]);
-    }
     function removeOneCharacter (index) {
       const updated = characters.filter((character, i) => {
           return i !== index
